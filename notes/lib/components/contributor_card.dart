717080e9f316fc788e098005f4b642a6132f8e6e
Gupta Shrinath <guptashrinath9@gmail.com>
2020-08-19 21:37:28 +0530

    Improve View

--- notes/lib/components/contributor_card.dart
+++ notes/lib/components/contributor_card.dart
@@ -17,96 +17,111 @@ class ContributorCard extends StatelessWidget {
           shadowColor: Color(0xFF000042),
           elevation: 6.0,
           //color: Colors.lightBlue,
-          child: ListTile(
-            leading: Image.asset(
-              ImageConstants.kAvatar,
-              height: 50.0,
-              width: 52.0,
-            ),
-            title: Padding(
-              padding: const EdgeInsets.only(top: 16),
-              child: Text(
-                'John Doe',
-                style: TextStyle(
-                  fontWeight: FontWeight.w900,
-                  fontSize: 17.0,
-                ),
-              ),
-            ),
-            subtitle: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  height: 10.0,
-                ),
-                Row(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ListTile(
+                title: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    Container(
-                      decoration: BoxDecoration(
-                        color: Color(0xFF009688),
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(
-                          'Developer',
-                          style: TextStyle(color: Colors.white),
+                    Image.asset(
+                      ImageConstants.kAvatar,
+                      height: 50.0,
+                      width: 52.0,
+                    ),
+                    SizedBox(
+                      width: 33.0,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(top: 16),
+                          child: Text(
+                            'John Doe',
+                            style: TextStyle(
+                              fontWeight: FontWeight.w900,
+                              fontSize: 17.0,
+                            ),
+                          ),
                         ),
-                      ),
-                    )
+                        SizedBox(
+                          height: 10.0,
+                        ),
+                        Row(
+                          children: [
+                            Container(
+                              decoration: BoxDecoration(
+                                color: Color(0xFF009688),
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.only(
+                                    left: 8.0, right: 8.0),
+                                child: Text(
+                                  'Developer',
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                        SizedBox(
+                          height: 10.0,
+                        ),
+                        Text(
+                          'College was the worst and \n the best thing',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            color: Color(0xFF009688),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10.0,
+                        ),
+                        Row(
+                          children: [
+                            Image.asset(
+                              ImageConstants.kGithub,
+                              height: 24,
+                              width: 24,
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Image.asset(
+                              ImageConstants.kTwitter,
+                              height: 24,
+                              width: 24,
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Image.asset(
+                              ImageConstants.kYoutube,
+                              height: 24,
+                              width: 24,
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Image.asset(
+                              ImageConstants.kSpotify,
+                              height: 24,
+                              width: 24,
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 16.0,
+                        ),
+                      ],
+                    ),
                   ],
                 ),
-                SizedBox(
-                  height: 10.0,
-                ),
-                Text(
-                  'College was the worst and the best thing',
-                  style: TextStyle(
-                    fontWeight: FontWeight.w600,
-                    color: Color(0xFF009688),
-                  ),
-                ),
-                SizedBox(
-                  height: 10.0,
-                ),
-                Row(
-                  children: [
-                    Image.asset(
-                      ImageConstants.kGithub,
-                      height: 24,
-                      width: 24,
-                    ),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Image.asset(
-                      ImageConstants.kTwitter,
-                      height: 24,
-                      width: 24,
-                    ),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Image.asset(
-                      ImageConstants.kYoutube,
-                      height: 24,
-                      width: 24,
-                    ),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Image.asset(
-                      ImageConstants.kSpotify,
-                      height: 24,
-                      width: 24,
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 16.0,
-                ),
-              ],
-            ),
+              )
+            ],
           )),
     );
   }

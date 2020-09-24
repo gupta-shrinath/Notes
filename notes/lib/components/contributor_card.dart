d72832bc62d2842b832ec28cd42624ec780efbe0
Gupta Shrinath <guptashrinath9@gmail.com>
2020-09-24 11:07:38 +0530

    Make message text responsive

--- notes/lib/components/contributor_card.dart
+++ notes/lib/components/contributor_card.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:notes/constants/image_constants.dart';
 
@@ -22,6 +23,7 @@ class ContributorCard extends StatelessWidget {
             children: [
               ListTile(
                 title: Row(
+                  mainAxisSize: MainAxisSize.min,
                   mainAxisAlignment: MainAxisAlignment.start,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
@@ -33,96 +35,110 @@ class ContributorCard extends StatelessWidget {
                     SizedBox(
                       width: 33.0,
                     ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(top: 16),
-                          child: Text(
-                            'John Doe',
-                            style: TextStyle(
-                              fontWeight: FontWeight.w900,
-                              fontSize: 17.0,
+                    Expanded(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(top: 16),
+                            child: Text(
+                              'John Doe',
+                              style: TextStyle(
+                                fontWeight: FontWeight.w900,
+                                fontSize: 17.0,
+                              ),
                             ),
                           ),
-                        ),
-                        SizedBox(
-                          height: 10.0,
-                        ),
-                        Row(
-                          children: [
-                            Container(
-                              decoration: BoxDecoration(
+                          SizedBox(
+                            height: 10.0,
+                          ),
+                          Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [Contribution()],
+                          ),
+                          SizedBox(
+                            height: 10.0,
+                          ),
+                          Flexible(
+                            child: Text(
+                              'College was the worst and the best thing',
+                              style: TextStyle(
+                                fontWeight: FontWeight.w600,
                                 color: Color(0xFF009688),
-                                borderRadius: BorderRadius.circular(20),
                               ),
-                              child: Padding(
-                                padding: const EdgeInsets.only(
-                                    left: 8.0, right: 8.0),
-                                child: Text(
-                                  'Developer',
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                              ),
-                            )
-                          ],
-                        ),
-                        SizedBox(
-                          height: 10.0,
-                        ),
-                        Text(
-                          'College was the worst and \n the best thing',
-                          style: TextStyle(
-                            fontWeight: FontWeight.w600,
-                            color: Color(0xFF009688),
+                            ),
                           ),
-                        ),
-                        SizedBox(
-                          height: 10.0,
-                        ),
-                        Row(
-                          children: [
-                            Image.asset(
-                              ImageConstants.kGithub,
-                              height: 24,
-                              width: 24,
-                            ),
-                            SizedBox(
-                              width: 10,
-                            ),
-                            Image.asset(
-                              ImageConstants.kTwitter,
-                              height: 24,
-                              width: 24,
-                            ),
-                            SizedBox(
-                              width: 10,
-                            ),
-                            Image.asset(
-                              ImageConstants.kYoutube,
-                              height: 24,
-                              width: 24,
-                            ),
-                            SizedBox(
-                              width: 10,
-                            ),
-                            Image.asset(
-                              ImageConstants.kSpotify,
-                              height: 24,
-                              width: 24,
-                            ),
-                          ],
-                        ),
-                        SizedBox(
-                          height: 16.0,
-                        ),
-                      ],
+                          SizedBox(
+                            height: 10.0,
+                          ),
+                          Row(
+                            children: [
+                              Image.asset(
+                                ImageConstants.kGithub,
+                                height: 24,
+                                width: 24,
+                              ),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              Image.asset(
+                                ImageConstants.kTwitter,
+                                height: 24,
+                                width: 24,
+                              ),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              Image.asset(
+                                ImageConstants.kYoutube,
+                                height: 24,
+                                width: 24,
+                              ),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              Image.asset(
+                                ImageConstants.kSpotify,
+                                height: 24,
+                                width: 24,
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 16.0,
+                          ),
+                        ],
+                      ),
                     ),
                   ],
                 ),
               )
             ],
           )),
+    );
+  }
+}
+
+class Contribution extends StatelessWidget {
+  const Contribution({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Color(0xFF009688),
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+        child: Text(
+          'Developer',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
     );
   }
 }

32b088de25543a38cca2551f9d4968e59914ffab
Gupta Shrinath <guptashrinath9@gmail.com>
2020-09-27 09:37:04 +0530

    Pass data through model class * Add function to open social pages

--- notes/lib/routes/contributors.dart
+++ notes/lib/routes/contributors.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:notes/components/contributor_card.dart';
 import 'package:notes/components/header.dart';
+import 'package:notes/model/contributor.dart';
 
 class Contributors extends StatelessWidget {
+  final Contributor contributor;
+  const Contributors({this.contributor});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,7 +21,9 @@ class Contributors extends StatelessWidget {
             Icon(Icons.arrow_back_ios)),
         body: ListView(
           children: [
-            ContributorCard(),
+            ContributorCard(
+              contributor: contributor,
+            ),
           ],
         ));
   }

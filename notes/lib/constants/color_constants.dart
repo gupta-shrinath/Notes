582d4228f6b1bc29ddbe46f7dfcf725c3127d893
Gupta Shrinath <guptashrinath9@gmail.com>
2020-10-01 10:33:54 +0530

    Create constants

---
+++ notes/lib/constants/color_constants.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+class ColorConstants {
+  static const Color primaryColor = Color(0xFF67349C);
+  static const Color accentColor = Color(0xFFDE6FA1);
+  // Colors used in lib/main.dart //
+  static const Color gray = Color(0XFFC9C7C7);
+  static const Color blueLight = Color(0XFF2196F3);
+  static const Color blueDark = Color(0xFF1565C0);
+  static const Color onBoardingIconDark = Color(0xFF003961);
+}

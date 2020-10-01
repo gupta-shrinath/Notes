a573c5e2d96eb177c9cf716fa6f90e49156d6a76
Gupta Shrinath <guptashrinath9@gmail.com>
2020-10-01 10:33:54 +0530

    Create constants

---
+++ notes/lib/constants/dimension_constants.dart
@@ -0,0 +1,4 @@
+class DimensionConstants {
+  static const double kAppbarHeight = 50.0;
+  static const double kBottomNavigationHeight = 70.0;
+}
---
+++ notes/lib/constants/text_style_constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class TextStyleConstants {
+  static const TextStyle kAppbarTextStyle = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+  );
+}

d9ad3bef7cb916d3cb40aff4265f3e934b4d2f43
Gupta Shrinath <guptashrinath9@gmail.com>
2020-10-01 10:27:09 +0530

    Follow design constraints

--- notes/lib/components/header.dart
+++ notes/lib/components/header.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:notes/constants/dimension_constants.dart';
 
-AppBar header(Text title, Widget leading) {
-  return AppBar(
-    title: title,
-    leading: leading,
-    centerTitle: true,
+Widget header(Text title, Widget leading) {
+  return PreferredSize(
+    preferredSize: Size.fromHeight(DimensionConstants.kAppbarHeight),
+    child: AppBar(
+      title: title,
+      leading: leading,
+      centerTitle: true,
+    ),
   );
 }
